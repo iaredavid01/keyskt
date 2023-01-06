a7ce1f876cb426cb2fdb15b4ff196ca2dbc1a6a2
David Ye <54700549+iaredavid01@users.noreply.github.com>
2023-01-06 14:10:47 -0800

    Update keysocket-spotify.js

--- extension/keysocket-spotify.js
+++ extension/keysocket-spotify.js
@@ -1,9 +1,7 @@
 keySocket.init(
     "spotify",
     {
-        "play-pause": ".spoticon-pause-16, .spoticon-play-16",
-        "prev": ".spoticon-skip-back-16",
-        "next": ".spoticon-skip-forward-16",
-        "stop": ".spoticon-pause-16"
+        "play-pause": ".vnCew8qzJq3cVGlYFXRI",
+        "next": ".mnipjT4SLDMgwiDCEnRC"
     }
 );
